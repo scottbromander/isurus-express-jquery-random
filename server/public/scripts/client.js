70e42c58e54d826576f3189afd2300057e54d07a
Scott Bromander <scottbromander@gmail.com>
2019-08-09 16:35:55 -0500

    update

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,25 @@
+const userInfo = {
+    name: null,
+    color: null
+}
+
+
 $(document).ready(init);
 
 function init() {
     console.log('Hello!');
 
     getDemo();
+
+    $('.js-btn-color').on('click', assignColor);
+    $('#js-btn-submit').on('click', submitUser);
+    $('#js-btn-submit').hide();
+}
+
+function assignColor() {
+    userInfo.color = $(this).data('color');
+    $('.js-btn-color').hide();
+    $('#js-btn-submit').show();
 }
 
 function getDemo() {
@@ -12,5 +28,46 @@ function getDemo() {
         url: '/demo'
     }).then((response) => {
         console.log(response);
+        reset();
+        render(response);
     });
+}
+
+function reset() {
+    $('.js-btn-color').show();
+    $('#js-btn-submit').hide();
+    $('#js-input-name').val('');
+}
+
+function render(arrayOfPeople) {
+    $('#js-container').empty();
+    for(let person of arrayOfPeople) {
+        $('#js-container').append(`
+            <div>
+                <p>${person.name} - ${person.color}</p>
+            </div>
+        `);
+    }
+}
+
+function submitUser() {
+    if($('#js-input-name').val() != '') {
+        userInfo.name = $('#js-input-name').val();
+        postData();
+    } else {
+        alert('Enter your name!');
+    }
+}
+
+function postData() {
+    userInfo.isUser = true;
+    userInfo.testNumber = 3;
+
+    $.ajax({
+        type: 'POST',
+        url: '/demo',
+        data: userInfo
+    }).then((response) => {
+        getDemo();
+    })
 }
--- server/server.js
+++ server/server.js
@@ -4,11 +4,27 @@ const bodyParser = require('body-parser');
 const PORT = 5000;
 
 app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const peopleArray = [];
 
 app.get('/demo', (req,res) => {
-    console.log('GET route!');
-    res.send('Got it!');
+    res.send(peopleArray);
 });
+
+app.post('/demo', (req,res) => {
+    console.log(req.body);
+
+    const personObject = {
+        name: req.body.name,
+        color: req.body.color,
+        isUser: req.body.isUser,
+        testNumber: parseInt(req.body.testNumber),
+    }
+
+    peopleArray.push(personObject);
+    res.sendStatus(201);
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
